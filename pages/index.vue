a369469da73ac3f54e6d61ac776bcb25eb0059a2
Kevin Restaino <kevinrestaino@gmail.com>
2017-07-17 18:39:59 -0500

    Only fetch home page posts if null

--- pages/index.vue
+++ pages/index.vue
@@ -20,8 +20,10 @@ export default {
   },
 
   async asyncData ({ store, params }) {
-    let posts = await axios.get('https://wp.kmr.io/wp-json/wp/v2/posts?orderby=date&per_page=10&_embed')
-    store.commit('setPosts', posts.data)
+    if (!store.state.posts) {
+      let posts = await axios.get('https://wp.kmr.io/wp-json/wp/v2/posts?orderby=date&per_page=10&_embed')
+      store.commit('setPosts', posts.data)
+    }
 
     if (!store.state.meta) {
       let meta = await axios.get('https://wp.kmr.io/wp-json')
