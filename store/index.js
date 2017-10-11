krestaino/nuepress

commit 727ffe5fbd819ded8a4a283eb6936ae4149b19f3

refactor: global meta fetch

store/index.js
@@ -2,6 +2,15 @@ import Vuex from 'vuex'
 
 const store = () => new Vuex.Store({
 
+  actions: {
+    nuxtServerInit ({ commit, state }) {
+      this.$axios.get(state.wordpressAPI)
+        .then((response) => {
+          commit('setMeta', response.data)
+        })
+    }
+  },
+
   state: {
     article: null,
     articles: [],

pages/_article.vue
@@ -45,11 +45,6 @@ export default {
   async asyncData ({ app, store, params }) {
     let article = await app.$axios.get(`${store.state.wordpressAPI}/wp/v2/posts?slug=${params.article}&_embed`)
     store.commit('setArticle', article.data[0])
-
-    if (!store.state.meta) {
-      let meta = await app.$axios.get(store.state.wordpressAPI)
-      store.commit('setMeta', meta.data)
-    }
   },
 
   beforeMount () {
@@ -97,9 +92,6 @@ export default {
           }
         </style>
       `
-    },
-    meta () {
-      return this.$store.state.meta
     }
   },
 
@@ -115,7 +107,7 @@ export default {
 
   head () {
     return {
-      title: `${this.article.title.rendered} | ${this.meta.name}`,
+      title: `${this.article.title.rendered} | ${this.$store.state.meta.name}`,
       meta: [
         { description: this.article.excerpt.rendered }
       ]

pages/authors/_author.vue
@@ -2,13 +2,13 @@
   <div class="author">
     <div class="articles">
       <div class="page-title">
-        <h1 v-html="author.name"></h1>
-        <p v-html="author.description"></p>
+        <h1>{{ author.name }}</h1>
+        <p v-if="author.description">{{ author.description }}</p>
       </div>
       <ArticleList :articles="authorArticles.articles"></ArticleList>
       <InfiniteLoading v-if="(authorArticles.infiniteLoading)  && (authorArticles.articles.length >= 10)" :on-infinite="moreArticles" ref="infiniteLoading"/>
     </div>
-    <Sidebar :featuredArticles="featuredArticles"></Sidebar>
+    <Sidebar :featuredArticles="$store.state.featuredArticles"></Sidebar>
   </div>
 </template>
 
@@ -35,11 +35,6 @@ export default {
       let authorArticles = await app.$axios.get(`${store.state.wordpressAPI}/wp/v2/posts?orderby=date&per_page=10&author=${author.id}&_embed`)
       store.commit('setAuthorArticles', {slug: params.author, articles: authorArticles.data, infiniteLoading: true, page: 1})
     }
-
-    if (!store.state.meta) {
-      let meta = await app.$axios.get(store.state.wordpressAPI)
-      store.commit('setMeta', meta.data)
-    }
   },
 
   components: {
@@ -58,23 +53,14 @@ export default {
       return find(this.$store.state.authorArticles, {
         'slug': this.$route.params.author
       })
-    },
-    authors () {
-      return this.$store.state.authors
-    },
-    featuredArticles () {
-      return this.$store.state.featuredArticles
-    },
-    meta () {
-      return this.$store.state.meta
     }
   },
 
   head () {
     return {
-      title: `${this.author.name} | ${this.meta.name}`,
+      title: `${this.author.name} | ${this.$store.state.meta.name}`,
       meta: [
-        { description: this.meta.description }
+        { description: this.$store.state.meta.description }
       ]
     }
   },
@@ -89,7 +75,6 @@ export default {
           this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded')
         })
         .catch(() => {
-          // this.authorArticles.infiniteLoading = false
           this.$refs.infiniteLoading.$emit('$InfiniteLoading:complete')
         })
     }

pages/authors/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page">
     <ul>
-      <li v-for="author in authors" :key="author.id">
+      <li v-for="author in $store.state.authors" :key="author.id">
         <nuxt-link :to="`/authors/${author.slug}`">
           <h2 v-html="author.name"></h2>
           <div v-html="author.description"></div>
@@ -18,27 +18,13 @@ export default {
       let authors = await app.$axios.get(`${store.state.wordpressAPI}/wp/v2/users?per_page=100`)
       store.commit('setAuthors', authors.data)
     }
-
-    if (!store.state.meta) {
-      let meta = await app.$axios.get(store.state.wordpressAPI)
-      store.commit('setMeta', meta.data)
-    }
-  },
-
-  computed: {
-    authors () {
-      return this.$store.state.authors
-    },
-    meta () {
-      return this.$store.state.meta
-    }
   },
 
   head () {
     return {
-      title: `Authors | ${this.meta.name}`,
+      title: `Authors | ${this.$store.state.meta.name}`,
       meta: [
-        { description: this.meta.description }
+        { description: this.$store.state.meta.description }
       ]
     }
   }

pages/index.vue
@@ -2,10 +2,10 @@
   <div class="home">
     <div class="articles">
       <Hero :heroArticle="heroArticle" v-if="heroArticle"/>
-      <ArticleList :articles="articles"/>
+      <ArticleList :articles="$store.state.articles"/>
       <InfiniteLoading v-if="indexInfiniteLoading.enabled" :on-infinite="moreArticles" ref="infiniteLoading"/>
     </div>
-    <Sidebar :featuredArticles="featuredArticles"/>
+    <Sidebar :featuredArticles="$store.state.featuredArticles"/>
   </div>
 </template>
 
@@ -26,11 +26,6 @@ export default {
       let articles = await app.$axios.get(`${store.state.wordpressAPI}/wp/v2/posts?orderby=date&per_page=10&categories=${store.state.featuredID}&_embed`)
       store.commit('setFeaturedArticles', articles.data)
     }
-
-    if (!store.state.meta) {
-      let meta = await app.$axios.get(store.state.wordpressAPI)
-      store.commit('setMeta', meta.data)
-    }
   },
 
   components: {
@@ -41,28 +36,19 @@ export default {
   },
 
   computed: {
-    articles () {
-      return this.$store.state.articles
-    },
     heroArticle () {
       return this.$store.state.articles[0]
     },
     indexInfiniteLoading () {
       return this.$store.state.indexInfiniteLoading
-    },
-    featuredArticles () {
-      return this.$store.state.featuredArticles
-    },
-    meta () {
-      return this.$store.state.meta
     }
   },
 
   head () {
     return {
-      title: `Home | ${this.meta.name}`,
+      title: `Home | ${this.$store.state.meta.name}`,
       meta: [
-        { description: this.meta.description }
+        { description: this.$store.state.meta.description }
       ]
     }
   },
@@ -78,7 +64,6 @@ export default {
         })
         .catch(() => {
           this.$refs.infiniteLoading.$emit('$InfiniteLoading:complete')
-          // this.$store.commit('setIndexInfiniteLoading', { enabled: false })
         })
     }
   }

pages/topics/_topic.vue
@@ -2,13 +2,13 @@
   <div class="topic">
     <div class="articles">
       <div class="page-title">
-        <h1 v-html="topic.name"></h1>
-        <p v-html="topic.description" v-if="topic.description"></p>
+        <h1>{{ topic.name }}</h1>
+        <p v-if="topic.description">{{ topic.description }}</p>
       </div>
       <ArticleList :articles="topicArticles.articles"></ArticleList>
       <InfiniteLoading v-if="(topicArticles.infiniteLoading)  && (topicArticles.articles.length >= 10)" :on-infinite="moreArticles" ref="infiniteLoading"/>
     </div>
-    <Sidebar :featuredArticles="featuredArticles"></Sidebar>
+    <Sidebar :featuredArticles="$store.state.featuredArticles"></Sidebar>
   </div>
 </template>
 
@@ -35,11 +35,6 @@ export default {
       let topicArticles = await app.$axios.get(`${store.state.wordpressAPI}/wp/v2/posts?orderby=date&per_page=10&categories=${topic.id}&_embed`)
       store.commit('setTopicArticles', {slug: params.topic, articles: topicArticles.data, infiniteLoading: true, page: 1})
     }
-
-    if (!store.state.meta) {
-      let meta = await app.$axios.get(store.state.wordpressAPI)
-      store.commit('setMeta', meta.data)
-    }
   },
 
   components: {
@@ -49,12 +44,6 @@ export default {
   },
 
   computed: {
-    featuredArticles () {
-      return this.$store.state.featuredArticles
-    },
-    meta () {
-      return this.$store.state.meta
-    },
     topic () {
       return find(this.$store.state.topics, {
         'slug': this.$route.params.topic
@@ -64,17 +53,14 @@ export default {
       return find(this.$store.state.topicArticles, {
         'slug': this.$route.params.topic
       })
-    },
-    topics () {
-      return this.$store.state.topic
     }
   },
 
   head () {
     return {
-      title: `${this.topic.name} | ${this.meta.name}`,
+      title: `${this.topic.name} | ${this.$store.state.meta.name}`,
       meta: [
-        { description: this.meta.description }
+        { description: this.$store.state.meta.description }
       ]
     }
   },
@@ -89,7 +75,6 @@ export default {
           this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded')
         })
         .catch(() => {
-          // this.topicArticles.infiniteLoading = false
           this.$refs.infiniteLoading.$emit('$InfiniteLoading:complete')
         })
     }

pages/topics/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page">
     <ul>
-      <li v-for="topic in topics" v-if="topic.slug !== 'featured'" :key="topic.id">
+      <li v-for="topic in $store.state.topics" v-if="topic.slug !== 'featured' && topic.count" :key="topic.id">
         <nuxt-link :to="`/topics/${topic.slug}`">
           <h2 v-html="topic.name"></h2>
           <div v-html="topic.description"></div>
@@ -18,27 +18,13 @@ export default {
       let topics = await app.$axios.get(`${store.state.wordpressAPI}/wp/v2/categories?per_page=100`)
       store.commit('setTopics', topics.data)
     }
-
-    if (!store.state.meta) {
-      let meta = await app.$axios.get(store.state.wordpressAPI)
-      store.commit('setMeta', meta.data)
-    }
-  },
-
-  computed: {
-    meta () {
-      return this.$store.state.meta
-    },
-    topics () {
-      return this.$store.state.topics
-    }
   },
 
   head () {
     return {
-      title: `Topics | ${this.meta.name}`,
+      title: `Topics | ${this.$store.state.meta.name}`,
       meta: [
-        { description: this.meta.description }
+        { description: this.$store.state.meta.description }
       ]
     }
   }

commit 65bd9317ff7b09bf8578569ced7e97dd3c0c6c3c

fix: regression from 727ffe5

store/index.js
@@ -3,11 +3,9 @@ import Vuex from 'vuex'
 const store = () => new Vuex.Store({
 
   actions: {
-    nuxtServerInit ({ commit, state }) {
-      this.$axios.get(state.wordpressAPI)
-        .then((response) => {
-          commit('setMeta', response.data)
-        })
+    async nuxtServerInit ({ commit, state }) {
+      let meta = await this.$axios.get(state.wordpressAPI)
+      commit('setMeta', meta.data)
     }
   },
 
@@ -23,7 +21,10 @@ const store = () => new Vuex.Store({
     featuredArticles: [],
     featuredColor: {},
     featuredID: 194,
-    meta: null,
+    meta: {
+      description: '',
+      name: ''
+    },
     topicArticles: [],
     topics: null,
     wordpressAPI: 'https://wp.kmr.io/wp-json'