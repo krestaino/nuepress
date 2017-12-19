188d33ef182e823a7589e76793df4f7c9f381825
Kevin Restaino <kevinrestaino@gmail.com>
2017-12-19 10:28:19 -0600

    fix(Comments): add loading spinner back

--- components/Comments.vue
+++ components/Comments.vue
@@ -1,26 +1,29 @@
 <template>
-<div class="comments">
-  <div class="loading" v-if="!disqusReady">
-    <Spinner1></Spinner1>
-    <span>Loading comments</span>
+  <div class="comments">
+    <div class="loading" v-if="!disqusReady">
+      <Spinner1></Spinner1>
+      <span>Loading comments</span>
+    </div>
+    <div class="disqus" :class="{ ready: disqusReady }">
+      <no-ssr>
+        <lazy-component>
+          <VueDisqus shortname="nuepress-kmr-io" :identifier="article.slug" @ready="disqusReady = true"></VueDisqus>
+        </lazy-component>
+      </no-ssr>
+    </div>
   </div>
-  <div class="disqus" :class="{ ready: disqusReady }">
-    <no-ssr>
-      <lazy-component>
-        <VueDisqus shortname="nuepress-kmr-io" :identifier="article.slug" @ready="disqusReady = true"></VueDisqus>
-      </lazy-component>
-    </no-ssr>
-  </div>
-</div>
 </template>
 
 <script>
+import Spinner1 from '~/components/Spinner1'
 import VueDisqus from 'vue-disqus/VueDisqus.vue'
+
 export default {
   props: {
     article: Object
   },
   components: {
+    Spinner1,
     VueDisqus
   },
   data () {
@@ -33,33 +36,33 @@ export default {
 
 <style lang="scss" scoped>
 .comments {
-    border-top: 1px dotted #65676a;
-    padding-top: 32px;
-    margin-top: 32px;
+  border-top: 1px dotted #65676a;
+  padding-top: 32px;
+  margin-top: 32px;
 
-    .loading {
-      align-items: center;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      position: absolute;
-      width: 100%;
+  .loading {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    width: 100%;
 
-      .spinner {
-        margin-bottom: 16px;
-      }
-    }
-
-    .disqus {
-      min-height: 440px;
-      opacity: 0;
-      transform: translateY(16px);
-      transition: 1s;
-
-      &.ready {
-        opacity: 1;
-        transform: translateY(0);
-      }
+    .spinner {
+      margin-bottom: 16px;
     }
   }
+
+  .disqus {
+    min-height: 440px;
+    opacity: 0;
+    transform: translateY(16px);
+    transition: 1s;
+
+    &.ready {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+}
 </style>
