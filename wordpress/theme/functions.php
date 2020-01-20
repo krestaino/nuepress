029d6b6ceedc054c4b133c01cd9d8dcb266cfb53
Kevin Restaino <kevinrestaino@gmail.com>
2020-01-19 23:42:47 -0600

    feat: hide customize theme options

--- wordpress/theme/functions.php
+++ wordpress/theme/functions.php
@@ -20,3 +20,15 @@ add_filter('rest_post_collection_params', 'increase_per_page_limit', 10, 2);
 add_filter('rest_page_collection_params', 'increase_per_page_limit', 10, 2);
 add_filter('rest_category_collection_params', 'increase_per_page_limit', 10, 2);
 add_filter('rest_user_collection_params', 'increase_per_page_limit', 10, 2);
+
+// Hide customize theme options
+function remove_customize_theme_options($wp_customize)
+{
+  $wp_customize->remove_section('colors');
+  $wp_customize->remove_section('header_image');
+  $wp_customize->remove_section('background_image');
+  $wp_customize->remove_panel('nav_menus');
+  $wp_customize->remove_section('static_front_page');
+  $wp_customize->remove_section('custom_css');
+}
+add_action('customize_register', 'remove_customize_theme_options', 50);
